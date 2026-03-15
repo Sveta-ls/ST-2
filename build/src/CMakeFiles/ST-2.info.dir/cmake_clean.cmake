1fa0b68ea51236107df71108b09887d7a1a4877e
Sveta-ls <slana6570@gmail.com>
2026-03-15 18:04:57 +0300

    cpp lint

---
+++ build/src/CMakeFiles/ST-2.info.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ST-2.info.dir/main.cpp.obj"
+  "CMakeFiles/ST-2.info.dir/main.cpp.obj.d"
+  "ST-2.info.exe"
+  "ST-2.info.exe.manifest"
+  "ST-2.info.pdb"
+  "libST-2.info.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ST-2.info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
